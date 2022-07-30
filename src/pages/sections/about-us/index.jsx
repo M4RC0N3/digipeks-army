c544507ecf965428987fe1d1ffd780fa2c0f8c4f
M4RC0N3 <marcone@quickup.io>
2022-07-30 18:57:25 -0300

    about in development

--- src/pages/sections/about-us/index.jsx
+++ src/pages/sections/about-us/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './style/index.css';
+import Lottie from 'react-lottie';
+import data from '../../../assets/animations/web-dev.json'
 const Index =()=>{
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: data,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
     return(
         <div className="about-us-container">
-            
+            <p>Section in development</p>
+            <div className="container">
+                <Lottie
+                options={defaultOptions}
+                />
+            </div>
         </div>
     );
 }
